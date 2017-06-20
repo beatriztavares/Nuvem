58f292529a59bcf07ae8ff7762723552a7af05c9
Beatriz Tavares <beatriztavares@gmail.com>
2017-06-20 19:34:50 -0300

    api deleted

--- ApiFamilyHealth/ApiFamilyHealth/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ApiFamilyHealth.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ApiFamilyHealth/README.md
+++
@@ -1,2 +0,0 @@
-# ApiFamilyHealth
-API do aplicativo Family Health
